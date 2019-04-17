47b9b9785705ba2ad0ea559231d9f694470e8082
Taihei <taiheipasocon@gmail.com>
2019-04-17 09:43:15 +0900

    sample画面の作成

--- src/main/kotlin/com/taihei/spring/controller/SampleController.kt
+++ src/main/kotlin/com/taihei/spring/controller/SampleController.kt
@@ -1,13 +1,24 @@
 package com.taihei.spring.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class SampleController(){
 
     @GetMapping("/sample")
-    fun sample(): String{
+    fun getSample(): String{
         return "sample"
+    }
+
+    @PostMapping("/sample")
+    fun postSample(
+            @RequestParam("sampleText") string: String, model: Model
+    ): String{
+        model.addAttribute("sampleValue", string)
+        return "sampleResponse"
     }
 }
